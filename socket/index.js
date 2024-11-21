a5c52a53e09db0757906d986c1d7b5c54f2e952c
ajayrajaloor <ajayrajaloor@gmail.com>
2024-11-21 16:06:22 +0530

    get user with token in socket and joined all online users and pass to client side

--- socket/index.js
+++ socket/index.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const {Server} = require("socket.io")
 const http = require("http")
+const getUserDetailFromToken = require("../helpers/getUserDetailFromToken")
 
 const app = express()
 
@@ -15,13 +16,28 @@ const io = new Server(server,{
 })
 
 
-io.on('connection',(socket)=>{
+//online user
+const onlineUser = new Set()
+
+io.on('connection',async(socket)=>{
         console.log('connected user', socket.id);
+
+        const token = socket.handshake.auth.token
+
+        //current user details
+        const user = await getUserDetailFromToken(token)
+
+        //create a room for the specific user to know if the users are online or not
+        socket.join(user?._id)
+        onlineUser.add(user?._id)
         
+        //pass the online users to the client side
+        io.emit('onlineUser',Array.from(onlineUser))
 
 
     //disconnect
-    io.on('disconnect', ()=>{
+    socket.on('disconnect', ()=>{
+        onlineUser.delete(user?._id)
         console.log('disconnected user',socket.id);
         
     })
