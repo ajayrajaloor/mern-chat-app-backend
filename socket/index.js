742471faaa4cba954c036960f82f18428c7086bd
ajayrajaloor <ajayrajaloor@gmail.com>
2024-11-21 18:08:52 +0530

    emitted the message-user to the client with payloads

--- socket/index.js
+++ socket/index.js
@@ -2,6 +2,7 @@ const express = require("express")
 const {Server} = require("socket.io")
 const http = require("http")
 const getUserDetailFromToken = require("../helpers/getUserDetailFromToken")
+const UserModel = require("../models/UserModel")
 
 const app = express()
 
@@ -29,10 +30,25 @@ io.on('connection',async(socket)=>{
 
         //create a room for the specific user to know if the users are online or not
         socket.join(user?._id)
-        onlineUser.add(user?._id)
+        onlineUser.add(user?._id?.toString())
         
         //pass the online users to the client side
         io.emit('onlineUser',Array.from(onlineUser))
+
+        socket.on('message-page',async(userId)=>{
+            console.log('userId',userId);
+            const userDetails = await UserModel.findById(userId).select("-password")
+
+            const payload = {
+                _id : userDetails?._id,
+                name : userDetails?.name,
+                email : userDetails?.email,
+                profile_pic : userDetails?.profile_pic,
+                online : onlineUser.has(userId)
+            }
+
+            socket.emit('message-user',payload)
+        })
 
 
     //disconnect
